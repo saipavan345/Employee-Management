aa549fc6acd8304337a4188c6657438a329fccfe
saipavan kumar <saipavan345>
2026-01-12 17:46:07 +0530

    Fix Vercel deployment config

--- Routes/AdminRoutes.js
+++ Routes/AdminRoutes.js
@@ -1,35 +1,4 @@
-// import express from "express";
-// import conn from "../utils/db.js";
-// import jwt from "jsonwebtoken";
 
-// const adminRouter = express.Router();
-
-// adminRouter.post("/adminlogin", (req, res) => {
-//   //console.log(req.body);
-//   //res.send("Admin login route working");
-//   const sql = "select * from admins where email=? and password=?";
-//   conn.query(sql, [req.body.email, req.body.password], (result, err) => {
-//     if (err) {
-//       return res.json({
-//         loginStatus: "false",
-//         err: "email and password not provided",
-//       });
-//     }
-//     if (result.length > 0) {
-//       const email = result[0].email;
-//       const token = jwt.sign({ role: "user" }, "jwt-secret-key", {
-//         expiresIn: "1d",
-//       });
-//       res.cookie("token", token);
-//       return res.json({
-//         loginStatus: "true",
-//       });
-//     }
-//     else return res.json({loginStatus:'false',err:"Wrong Credentails"})
-//   });
-// });
-
-// export default adminRouter;
 
 import express from "express";
 import conn from "../utils/db.js";
